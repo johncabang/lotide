2735191b723f7110e59a1dbec36174dfdca2cb72
johncabang <johncabz@gmail.com>
2020-09-16 01:00:25 +0000

    Added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    let e1 = arr1[i];
+    let e2 = arr2[i];
+    if (e1 !== e2) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log('Arrays are equal.');
+  } else {
+    console.log('Arrays are not equal.');
+  }
+};
+
+assertArraysEqual([1, 3, 3], [1, 3, 3]);
