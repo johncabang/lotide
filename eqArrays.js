0e4dd6bab665c842e9b72f6326a42d6b1266185f
johncabang <johncabz@gmail.com>
2020-09-16 00:16:51 +0000

    Added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else (console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`));
+};
+
+// assertEqual('Lighthouse Labs', 'Bootcamp');
+// assertEqual(1, 1);
+
+
+
+// Implement eqArrays
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    let e1 = arr1[i];
+    let e2 = arr2[i];
+    if (e1 !== e2) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+// Test Case:
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+ 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
