edafb08e23ec1716a9f720461c0fe03c50f203d2
johncabang <johncabz@gmail.com>
2020-09-17 12:40:26 +0000

    Added explanation notes

--- eqArrays.js
+++ eqArrays.js
@@ -13,6 +13,7 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
+    // console.log(arr1.length, arr2.length); // test to check length of arr1 & arr2
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
@@ -27,6 +28,7 @@ const eqArrays = function(arr1, arr2) {
 
 
 // Test Case:
+console.log(eqArrays([1, 2, 3, 4], [1, 2, 3])); // => true
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
