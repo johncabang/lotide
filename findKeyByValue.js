56d1be45af65a5c293c131e18099adffb643a7fa
johncabang <johncabz@gmail.com>
2020-09-17 01:26:42 +0000

    Updated function

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -4,6 +4,29 @@ const assertEqual = function(actual, expected) {
   } else (console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`));
 };
 
-const findKeyByValue = function() {
-
+const findKeyByValue = function(object, value) {
+  const keysArray = Object.keys(object);
+  for (let element of keysArray) {
+    if (object[element] === value) {
+      return element;
+    }
+  }
 };
+
+const bestTVShowsByGenre = {
+  sci_fi: 'Stranger Things',
+  comedy: 'Brooklyn Nine-Nine',
+  drama: 'The Wire',
+  sitcom: 'Saved by the Bell',
+  reality: 'Survivor'
+};
+
+// Testing Object.keys
+// console.log(Object.keys(bestTVShowsByGenre));  
+
+// Test Case:
+
+console.log(findKeyByValue(bestTVShowsByGenre, "Stranger Things"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
