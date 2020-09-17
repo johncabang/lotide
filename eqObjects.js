40536bdec1e4cbf889d7c57cfffa120da455b286
johncabang <johncabz@gmail.com>
2020-09-17 02:00:52 +0000

    Added test cases

--- eqObjects.js
+++ eqObjects.js
@@ -5,5 +5,16 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqObjects = function(object1, object2) {
-  
+
 }
+
+
+
+// Test Case: Primitives as values
+
+const ab = { a: '1', b: '2' };
+const ba = { b: '2', a: '1' };
+console.log(eqObjects(ab, ba)); // returns true
+
+const abc = { a: '1', b: '2', c: '3' };
+console.log(eqObjects(ab, abc)); // returns false
