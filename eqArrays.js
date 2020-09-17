c7070d270cfbb317ff8c5372e1e787a87ed87569
johncabang <johncabz@gmail.com>
2020-09-17 12:52:55 +0000

    Added more explanation notes

--- eqArrays.js
+++ eqArrays.js
@@ -13,13 +13,14 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    // console.log(arr1.length, arr2.length); // test to check length of arr1 & arr2
+    // console.log(arr1.length, arr2.length); // test: check length of arr1 & arr2
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    let e1 = arr1[i];
-    let e2 = arr2[i];
-    if (e1 !== e2) {
+    // console.log('*inside for loop*', arr1[i], '**', arr2[i]); // test: check arr1[i], arr2[i] values
+    // let e1 = arr1[i]; // create variables for arr1[i], arr2[i]
+    // let e2 = arr2[i];
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
@@ -28,12 +29,12 @@ const eqArrays = function(arr1, arr2) {
 
 
 // Test Case:
-console.log(eqArrays([1, 2, 3, 4], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3, 4], [1, 2, 3])); // returns> true
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // returns true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // returns false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // returns true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // returns false
  
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // returns should PASS
