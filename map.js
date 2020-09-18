6039a6b7da8b6ec9af59b16b5ada9ac394635895
johncabang <johncabz@gmail.com>
2020-09-18 00:01:26 +0000

    Created map.js

---
+++ map.js
@@ -0,0 +1,12 @@
+// Map function, takes in two arguments - an array to map, a callback function
+// Map function will return a new array based on the results of the callback function
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(mapArray, callback) {
+  const results = [];
+};
+
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
